f8e185ebd07929bb09dd49ec771c4bbed2cd7d89
Jakub Votrubec <votruja6@fit.cvut.cz>
2025-10-10 10:10:04 +0200

    AABB exmansion

--- src/aabb.h
+++ src/aabb.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #include "src/vec3.h"
 
 namespace raytracer {
@@ -13,6 +15,15 @@ struct AABB {
     AABB(const Point3& min, const Point3& max) : min(min), max(max), size(abs(max - min)) {}
 
     float volume() const { return (max.x - min.x) * (max.y - min.y) * (max.z - min.z); }
+    void expand(const AABB& other) {
+        min.x = std::min(min.x, other.min.x);
+        min.y = std::min(min.y, other.min.y);
+        min.z = std::min(min.z, other.min.z);
+        max.x = std::max(max.x, other.max.x);
+        max.y = std::max(max.y, other.max.y);
+        max.z = std::max(max.z, other.max.z);
+        size = max - min;
+    }
 };
 
 }  // namespace raytracer
