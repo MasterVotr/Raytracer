baaedf260a647d47fc2427c2c0a7434867f03a8d
Jakub Votrubec <votruja6@fit.cvut.cz>
2025-12-15 09:30:42 +0100

    Fixed timer to be header only compatible

--- src/timer.h
+++ src/timer.h
@@ -4,46 +4,22 @@
 #include <ostream>
 
 class Timer {
-    public:
-        Timer();
-        Timer(std::string name);
-        inline double elapsed_s() const;
-        inline double elapsed_ms() const;
-        inline double elapsed_μs() const;
-        inline uint64_t elapsed_ns() const;
-        inline const std::string& name() const;
-        inline void reset();
-    private:
-        std::chrono::high_resolution_clock::time_point start_;
-        std::string name_;
+   public:
+    Timer() : Timer("") {}
+    Timer(std::string name) : start_(std::chrono::high_resolution_clock::now()), name_(name) {}
+
+    inline double elapsed_s() const { return elapsed_ns() / 1000000000.0; }
+    inline double elapsed_ms() const { return elapsed_ns() / 1000000.0; }
+    inline double elapsed_μs() const { return elapsed_ns() / 1000.0; }
+    inline uint64_t elapsed_ns() const {
+        auto end = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start_).count();
+    }
+
+    inline const std::string& name() const { return name_; }
+    inline void reset() { start_ = std::chrono::high_resolution_clock::now(); }
+
+   private:
+    std::chrono::high_resolution_clock::time_point start_;
+    std::string name_;
 };
-
-Timer::Timer() : Timer("") {}
-
-Timer::Timer(std::string name) : start_(std::chrono::high_resolution_clock::now()), name_(name) {}
-
-double Timer::elapsed_s() const {
-    return elapsed_ns() / 1000000000.0;
-}
-
-double Timer::elapsed_ms() const {
-    return elapsed_ns() / 1000000.0;
-}
-
-double Timer::elapsed_μs() const {
-    return elapsed_ns() / 1000.0;
-}
-
-uint64_t Timer::elapsed_ns() const {
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start_).count();
-}
-
-const std::string& Timer::name() const {
-    return name_;
-}
-
-void Timer::reset() {
-    start_ = std::chrono::high_resolution_clock::now();
-}
-
